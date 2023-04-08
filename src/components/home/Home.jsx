e36e64960c07f686e1327ebb5f51d1a58ba3005c
Sayem92 <sayemmiha123@gmail.com>
2023-04-08 15:41:54 +0600

    add banner

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../header/Header";
 import Profile from "../profile/Profile";
+import Banner from "./banner/Banner";
 
 const Home = () => {
   return (
     <div>
-      <Header></Header>
-      <Profile></Profile>
+      {/* <Header></Header>
+      <Profile></Profile> */}
+      <Banner></Banner>
     </div>
   );
 };
