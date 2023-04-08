a5147b3dc7ad6bdf96c5235de0633a260a0c9db9
Sayem92 <sayemmiha123@gmail.com>
2023-04-08 21:33:02 +0600

    about me data save

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
     window.location.reload();
   };
 
-  console.log(user);
+
   return (
     <div className="bg-gray-100 ">
       <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
--- src/components/profile/AboutMe.jsx
+++ src/components/profile/AboutMe.jsx
@@ -1,13 +1,51 @@
 import React, { useState } from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { AuthContext } from "../../context/AuthProvider";
 
 const AboutMe = () => {
+  const { user } = useContext(AuthContext);
   const [open, setOpen] = useState(true);
   const [about, setAbout] = useState("");
 
   const handleSubmit = () => {
     setOpen(!open);
-    console.log(about);
+    // console.log(about);
+
+    if (!open) {
+      const aboutMe = {
+        about: about,
+      };
+      
+      // sava information to the database----------
+      fetch(`http://localhost:5000/profile/${user?.email}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(aboutMe),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data) {
+            localStorage.setItem("about", JSON.stringify(aboutMe));
+          }
+        });
+    }
   };
+
+  useEffect(() => {
+    if (!about) {
+      const value = JSON.parse(localStorage.getItem("about"));
+      setAbout(value?.about);
+    }
+  }, [about]);
+
+  const handleChange = (event) => {
+    setAbout(event);
+  };
+
   return (
     <div className="mx-12 my-8">
       <div className="flex justify-between items-center mb-5">
@@ -23,9 +61,9 @@ const AboutMe = () => {
         readOnly={open}
         className="w-full resize-none h-32 p-2 outline-none rounded-md"
         type="text"
-        value={about}
+        defaultValue={about}
         placeholder="Add something about you."
-        onChange={(e) => setAbout(e.target.value)}
+        onChange={(e) => handleChange(e.target.value)}
       />
     </div>
   );
