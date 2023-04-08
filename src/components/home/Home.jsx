69fc83ef892ddfa3b1389927c0f05320cc8f77bf
Sayem92 <sayemmiha123@gmail.com>
2023-04-08 16:22:35 +0600

    add up coming page and banner

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Header from "../header/Header";
-import Profile from "../profile/Profile";
+// import Header from "../header/Header";
+// import Profile from "../profile/Profile";
 import Banner from "./banner/Banner";
+import Courses from "./courses/Courses";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       {/* <Header></Header>
       <Profile></Profile> */}
       <Banner></Banner>
+      <Courses></Courses>
     </div>
   );
 };
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,27 +4,34 @@ import Main from "../layout/Main";
 import Home from "../components/home/Home";
 import Register from "../components/form/Register";
 import Login from "../components/form/Login";
+import Upcoming from "../components/upcoming/Upcoming";
 
-export const router = createBrowserRouter([{
-    path: '/',
-    element:<Main></Main>,
-    errorElement:<ErrorPage></ErrorPage>,
-    children:[
-        {
-            path:'/',
-            element:<Home></Home>
-        },
-        {
-            path:'/home',
-            element:<Home></Home>
-        },
-        {
-            path:'/register',
-            element:<Register></Register>
-        },
-        {
-            path:'/login',
-            element:<Login></Login>
-        },
-    ]
-}])
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/courses/:coursesName",
+        element: <Upcoming></Upcoming>,
+      },
+    ],
+  },
+]);
